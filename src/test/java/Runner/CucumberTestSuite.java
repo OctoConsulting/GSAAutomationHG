accb2a27524446590b0f7a2c3fb9eb7d822bc42e
mahammadabasguliyev <mahammadabasguliyev@I04BMC-GSU1024.ent.ds.gsa.gov>
2020-11-23 02:33:45 -0500

    chromedriver updated

--- src/test/java/Runner/CucumberTestSuite.java
+++ src/test/java/Runner/CucumberTestSuite.java
@@ -8,9 +8,9 @@ import org.junit.runner.RunWith;
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
 		plugin = { "pretty" }, 
-		features = "src\\test\\resources\\features-Integration5\\integration-FH2",
+		features = "src\\test\\resources\\features\\FH2\\OfficeMoveValidationsWithEndDate.feature",
 		glue = {"stepdefinitions"},
-		//tags =  {"@IntegrationFH"},
+		//tags =  {"@FH2"},
 		monochrome = true,
         dryRun = false)
 
